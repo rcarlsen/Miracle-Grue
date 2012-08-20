4fbc5a5920cfc36ab3fa7cb3d4bc3a06aaae97a3
Filipp Gelman <filipp@makerbot.com>
2012-08-20 09:32:07 -0400

    Whitespace change

--- src/mgl/pather.h
+++ src/mgl/pather.h
@@ -7,7 +7,7 @@
    published by the Free Software Foundation, either version 3 of the
    License, or (at your option) any later version.
 
-*/
+ */
 
 
 #ifndef PATHER_H_
@@ -18,6 +18,7 @@
 #include "regioner.h"
 #include "loop_path.h"
 #include "labeled_path.h"
+#include "layer_label.h"
 
 #include <list>
 
@@ -25,102 +26,111 @@ namespace mgl {
 
 class PatherConfig {
 public:
-	PatherConfig() 
-			: doGraphOptimization(true), 
-			coarseness(0.05), 
-			directionWeight(1.0), 
-                        doRaft(false), 
-                        raftLayers(0){}
-	bool doGraphOptimization;
-	Scalar coarseness;
-	Scalar directionWeight;
-        bool doRaft;
-        unsigned int raftLayers;
+
+    PatherConfig()
+    : doGraphOptimization(true),
+    coarseness(0.05),
+    directionWeight(1.0),
+    doRaft(false),
+    raftLayers(0) {
+    }
+    bool doGraphOptimization;
+    Scalar coarseness;
+    Scalar directionWeight;
+    bool doRaft;
+    unsigned int raftLayers;
 };
 
 typedef std::vector<LoopList> InsetVector; // TODO: make this a smarter object
 
 // slice data for an extruder
+
 class ExtruderConfig {
 public:
-	unsigned int defaultExtruder;
+    unsigned int defaultExtruder;
 };
-
 
 class ExtruderSlice {
 public:
 
-	Polygons boundary; // boundary loops for areas of this slice of a print.
-	Polygons infills; // list of all lines that create infill for this layer
-	PolygonsGroup insetLoopsList; /// a list, each entry of which is a Polygons
-	/// object. Each inset[i] is all shell polygons
-	/// for the matching loops[i] boundary for this layer
+    Polygons boundary; // boundary loops for areas of this slice of a print.
+    Polygons infills; // list of all lines that create infill for this layer
+    PolygonsGroup insetLoopsList; /// a list, each entry of which is a Polygons
+    /// object. Each inset[i] is all shell polygons
+    /// for the matching loops[i] boundary for this layer
 };
 
-class LayerPaths{
+class LayerPaths {
 public:
-	class Layer;
-	typedef std::list<Layer> LayerList;
-	typedef LayerList::iterator layer_iterator;
-	typedef LayerList::const_iterator const_layer_iterator;
-	
-	class Layer{
-	public:
-		class ExtruderLayer;
-		typedef std::list<ExtruderLayer> ExtruderList;
-		typedef ExtruderList::iterator extruder_iterator;
-		typedef ExtruderList::const_iterator const_extruder_iterator;
-		class ExtruderLayer{
-		public:
+    class Layer;
+    typedef std::list<Layer> LayerList;
+    typedef LayerList::iterator layer_iterator;
+    typedef LayerList::const_iterator const_layer_iterator;
+
+    class Layer {
+    public:
+        class ExtruderLayer;
+        typedef std::list<ExtruderLayer> ExtruderList;
+        typedef ExtruderList::iterator extruder_iterator;
+        typedef ExtruderList::const_iterator const_extruder_iterator;
+
+        class ExtruderLayer {
+        public:
             static const int OUTLINE_LABEL_VALUE = 10;
-			typedef std::list<OpenPathList> InsetList;
-			typedef std::list<OpenPath> InfillList;
-			typedef std::list<OpenPath> OutlineList;
-			typedef std::list<LabeledOpenPath> LabeledPathList;
-			typedef InsetList::iterator inset_iterator;
-			typedef InfillList::iterator infill_iterator;
-			typedef OutlineList::iterator outline_iterator;
-			typedef LabeledPathList::iterator path_iterator;
-			typedef InsetList::const_iterator const_inset_iterator;
-			typedef InfillList::const_iterator const_infill_iterator;
-			typedef OutlineList::const_iterator const_outline_iterator;
-			typedef LabeledPathList::const_iterator const_path_iterator;
-			ExtruderLayer(size_t exId = 0) : extruderId(exId) {}
-			InsetList insetPaths;
-			InfillList infillPaths;
-			InfillList supportPaths;
-			OutlineList outlinePaths;
-			LabeledPathList paths;
-			size_t extruderId;
-		};
-		Layer(Scalar z, Scalar layerh, Scalar layerw, layer_measure_index_t mind)
-				: layerZ(z), layerHeight(layerh), layerW(layerw), 
-				measure_index(mind) {}
-		Layer(layer_measure_index_t mind = 0) 
-				: layerZ(0), layerHeight(0.27), layerW(0.43), measure_index(mind) {}
-		ExtruderList extruders;
-		Scalar layerZ;		//vertical coordinate
-		Scalar layerHeight;	//thickness
-		Scalar layerW;		//width of filament
-		layer_measure_index_t measure_index;
-	};
-	
-	layer_iterator begin();
-	const_layer_iterator begin() const;
-	layer_iterator end();
-	const_layer_iterator end() const;
-	void push_back(const Layer& value);
-	void push_front(const Layer& value);
-	void pop_back();
-	void pop_front();
-	layer_iterator insert(layer_iterator at, const Layer& value);
-	layer_iterator erase(layer_iterator at);
-	layer_iterator erase(layer_iterator from, layer_iterator to);
-	bool empty() const;
-	size_t layerCount() const;
-	Layer& back();
+            typedef std::list<OpenPathList> InsetList;
+            typedef std::list<OpenPath> InfillList;
+            typedef std::list<OpenPath> OutlineList;
+            typedef std::list<LabeledOpenPath> LabeledPathList;
+            typedef InsetList::iterator inset_iterator;
+            typedef InfillList::iterator infill_iterator;
+            typedef OutlineList::iterator outline_iterator;
+            typedef LabeledPathList::iterator path_iterator;
+            typedef InsetList::const_iterator const_inset_iterator;
+            typedef InfillList::const_iterator const_infill_iterator;
+            typedef OutlineList::const_iterator const_outline_iterator;
+            typedef LabeledPathList::const_iterator const_path_iterator;
+
+            ExtruderLayer(size_t exId = 0) : extruderId(exId) {
+            }
+            InsetList insetPaths;
+            InfillList infillPaths;
+            InfillList supportPaths;
+            OutlineList outlinePaths;
+            LabeledPathList paths;
+            size_t extruderId;
+        };
+
+        Layer(Scalar z, Scalar layerh, Scalar layerw, layer_measure_index_t mind)
+        : layerZ(z), layerHeight(layerh), layerW(layerw),
+        measure_index(mind) {
+        }
+
+        Layer(layer_measure_index_t mind = 0)
+        : layerZ(0), layerHeight(0.27), layerW(0.43), measure_index(mind) {
+        }
+        ExtruderList extruders;
+        Scalar layerZ; //vertical coordinate
+        Scalar layerHeight; //thickness
+        Scalar layerW; //width of filament
+        layer_measure_index_t measure_index;
+    };
+
+    layer_iterator begin();
+    const_layer_iterator begin() const;
+    layer_iterator end();
+    const_layer_iterator end() const;
+    void push_back(const Layer& value);
+    void push_front(const Layer& value);
+    void pop_back();
+    void pop_front();
+    layer_iterator insert(layer_iterator at, const Layer& value);
+    layer_iterator erase(layer_iterator at);
+    layer_iterator erase(layer_iterator from, layer_iterator to);
+    bool empty() const;
+    size_t layerCount() const;
+    Layer& back();
 private:
-	LayerList layers;
+    LayerList layers;
 };
 
 ::std::ostream& operator<<(::std::ostream& os, const ExtruderSlice& x);
@@ -130,74 +140,78 @@ typedef std::vector<ExtruderSlice > ExtruderSlices;
 /// The Slice data is contains polygons
 /// for each extruder, for a slice.
 /// there are multiple polygons for each extruder
-class SliceData
-{
+
+class SliceData {
 private:
-	Scalar zHeight;
-	size_t index;
+    Scalar zHeight;
+    size_t index;
 
 public:
-	ExtruderSlices extruderSlices;
+    ExtruderSlices extruderSlices;
 
-	/// @param inHeight: z height of this layer. Middle of the specified layer
-	/// @param inIndex: layer number in this  model, positive in the 'up' direction
-	SliceData(Scalar inHeight=0, size_t inIndex=0):zHeight(inHeight), index(inIndex)
-	{
-	}
+    /// @param inHeight: z height of this layer. Middle of the specified layer
+    /// @param inIndex: layer number in this  model, positive in the 'up' direction
 
-	/// Updates position of slice in a model
-	/// @param inHeight: z height of this layer. Middle of the specified layer
-	/// @param inIndex: layer number in this  model, positive in the 'up' direction
-	void updatePosition(Scalar inHeight,size_t inIndex){
-		zHeight = inHeight;
-		index = inIndex ;
-	}
+    SliceData(Scalar inHeight = 0, size_t inIndex = 0) : zHeight(inHeight), index(inIndex) {
+    }
 
-	Scalar getZHeight() const { return zHeight;}
-	size_t getIndex()const  { return index;}
+    /// Updates position of slice in a model
+    /// @param inHeight: z height of this layer. Middle of the specified layer
+    /// @param inIndex: layer number in this  model, positive in the 'up' direction
+
+    void updatePosition(Scalar inHeight, size_t inIndex) {
+        zHeight = inHeight;
+        index = inIndex;
+    }
+
+    Scalar getZHeight() const {
+        return zHeight;
+    }
+
+    size_t getIndex()const {
+        return index;
+    }
 
 
 };
 
 ::std::ostream& operator<<(::std::ostream& os, const SliceData& x);
 
-
-class Pather : public Progressive
-{
+class Pather : public Progressive {
 private:
-	PatherConfig patherCfg;
+    PatherConfig patherCfg;
 
 public:
 
 
-	Pather(const PatherConfig& pCfg, ProgressBar * progress = NULL);
+    Pather(const PatherConfig& pCfg, ProgressBar * progress = NULL);
 
 
-	void generatePaths(const ExtruderConfig &extruderCfg,
-					   const RegionList &skeleton,
-					   const LayerMeasure &layerMeasure,
-					   const Grid &grid,
-					   LayerPaths &slices,
-					   int sfirstSliceIdx=-1,
-					   int slastSliceIdx=-1);
+    void generatePaths(const ExtruderConfig &extruderCfg,
+            const RegionList &skeleton,
+            const LayerMeasure &layerMeasure,
+            const Grid &grid,
+            LayerPaths &slices,
+            int sfirstSliceIdx = -1,
+            int slastSliceIdx = -1);
 
 
-	void outlines(const LoopList& outline_loops,
-				  LoopPathList &boundary_paths);
+    void outlines(const LoopList& outline_loops,
+            LoopPathList &boundary_paths);
 
-	void insets(const std::list<LoopList>& insetsForSlice,
-				std::list<LoopPathList> &insetPaths);
+    void insets(const std::list<LoopList>& insetsForSlice,
+            std::list<LoopPathList> &insetPaths);
 
-	void infills(const GridRanges &infillRanges,
-				 const Grid &grid,
-				 const LoopList& outlines,
-				 bool direction,
-				 OpenPathList &infills);
-	
-	void directionalCoarsenessCleanup(
-		LayerPaths::Layer::ExtruderLayer::LabeledPathList& labeledPaths);
-	void directionalCoarsenessCleanup(LabeledOpenPath& labeledPath);
-	
+    void infills(const GridRanges &infillRanges,
+            const Grid &grid,
+            const LoopList& outlines,
+            bool direction,
+            OpenPathList &infills);
+
+    void directionalCoarsenessCleanup(
+            LayerPaths::Layer::ExtruderLayer::LabeledPathList& labeledPaths);
+    void directionalCoarsenessCleanup(LabeledOpenPath& labeledPath);
+
 
 };
 
